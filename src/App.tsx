bcffabd1ae1f80602f3c8958f829ad1d6ca22d60
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2022-07-17 00:06:22 -0300

    Configuracao da base da estilizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <h1>Aplicação Web</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Aplicação Web</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
