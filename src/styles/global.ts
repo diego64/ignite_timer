a4a7ddeafc332874c976828d32684e863f7086b5
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2022-10-03 19:59:14 -0300

    Desenvolvimento da pagina home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,17 +6,15 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
   :focus {
-    outline: 0;
-    box:shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
-
   body {
-    background: ${(props) => props.theme['gray-800']};
+    background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
-
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
